7bc9e5b05f88ebc88f56922268e4b704fae3337c
Filip Baszak <baszak.filip@gmail.com>
2018-11-24 20:52:20 +0100

    Updated shop controller

--- src/main/java/inz/controller/PromotionController.java
+++ src/main/java/inz/controller/PromotionController.java
@@ -1,4 +1,10 @@
 package inz.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/promotion")
 public class PromotionController {
+	
 }
--- src/main/java/inz/controller/ShopController.java
+++ src/main/java/inz/controller/ShopController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import inz.model.Shop;
 import inz.repository.ShopRepository;
 
+@RestController
+@RequestMapping("/api/shop")
 public class ShopController {
 	
 	@Autowired
