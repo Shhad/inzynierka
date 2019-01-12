e49c6f1ad07ce677e100123298dca59c328d34bf
Filip <baszak.filip@gmail.com>
2019-01-12 15:25:11 +0100

    Fixed production addresss

--- web-app/src/constants/AppConstants.js
+++ web-app/src/constants/AppConstants.js
@@ -2,4 +2,4 @@
 
 export const APP_VERSION = '0.1.0';
 export const SERWER_LOCAL = 'http://localhost:8080';
-export const SERWER_PROD = 'http://35.203.103.21:8080';
+export const SERWER_PROD = 'http://35.228.103.221:8080';
